4c4d56662c18d1ecfc8d7fffa516c2a0e616a808
maxie <maxiperezc@gmail.com>
2019-04-11 12:33:03 -0300

    creating in memory db for sqlite3 tests

--- spec/tester_ar/config/environment.rb
+++ spec/tester_ar/config/environment.rb
@@ -5,3 +5,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if ENV["RAILS_ENV"] == "test"
+  puts "creating sqlite in memory database"
+  APP_ROOT = Pathname.new File.expand_path('..', __dir__)
+  load "#{APP_ROOT}/db/schema.rb" # use db agnostic schema by default
+  # ActiveRecord::Migrator.up('db/migrate') # use migrations
+end
