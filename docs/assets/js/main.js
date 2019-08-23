063c76330e2da71233c6540c4cdcae8cef8651c0
maxiperezc <maxiperezc@gmail.com>
2019-08-23 11:26:38 -0300

    code style

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,22 +3,11 @@ $(document).ready(function() {
   /* Activate scrollspy menu */
   $('body').scrollspy({target: '#doc-menu', offset: 100});
 
-    /* Smooth scrolling */
+  /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
-        //store hash
-        var target = this.hash;
-        e.preventDefault();
+    //store hash
+    var target = this.hash;
+    e.preventDefault();
 		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
-
 	});
-
-    /* Bootstrap lightbox */
-    /* Ref: http://ashleydw.github.io/lightbox/ */
-
-    // $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
-    //     e.preventDefault();
-    //     $(this).ekkoLightbox();
-    // });    
-
-
 });
