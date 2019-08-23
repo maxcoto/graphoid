237c169db82c1818ce30aaedd27758e152307db1
maxiperezc <maxiperezc@gmail.com>
2019-08-23 10:38:10 -0300

    fixing docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
 
   /* Activate scrollspy menu */
   $('body').scrollspy({target: '#doc-menu', offset: 100});
-  
+
     /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
         //store hash
-        var target = this.hash;    
+        var target = this.hash;
         e.preventDefault();
 		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
-		
+
 	});
-     
+
     /* Bootstrap lightbox */
     /* Ref: http://ashleydw.github.io/lightbox/ */
 
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
-        e.preventDefault();
-        $(this).ekkoLightbox();
-    });    
+    // $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
+    //     e.preventDefault();
+    //     $(this).ekkoLightbox();
+    // });    
 
 
 });
