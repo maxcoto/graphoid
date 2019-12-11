227dff018b7726aa0f36d9b2a56016e292560b8e
maxie <maxiperezc@gmail.com>
2019-12-11 12:56:44 -0300

    skip before action

--- spec/tester_ar/app/controllers/graphql_controller.rb
+++ spec/tester_ar/app/controllers/graphql_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GraphqlController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
